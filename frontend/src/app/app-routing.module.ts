8f1fab00415c1f1b56f0449ba3b26acdcddaf713
RenatoXD4 <re.chicahual@duocuc.cl>
2023-10-16 22:20:10 -0300

    Probando creación de rutas y componentes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'inicio',
+    loadChildren: () => import('./views/inicio/inicio-routing.module').then(m => m.InicioRoutingModule)
+  },
+  {
+    path: '',
+    redirectTo: 'inicio',
+    pathMatch: 'full'
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { InicioComponent } from './views/inicio/inicio.component';
+import { ServiciosComponent } from './views/servicios/servicios.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    InicioComponent,
+    ServiciosComponent
   ],
   imports: [
     BrowserModule,
