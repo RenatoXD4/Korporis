8704afc5165050411c2d8cdafe9646f3bb1a6d5e
hksseba <seb.silvan@duocuc.cl>
2023-11-29 01:57:30 -0300

    a
    a

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './layouts/navbar/navbar.component';
-import { ServiciosComponent } from './views/servicios/servicios.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -14,7 +13,6 @@ import { HttpClientModule } from '@angular/common/http';
   declarations: [
     AppComponent,
     NavbarComponent,
-    ServiciosComponent,
     FooterComponent
   ],
   imports: [
