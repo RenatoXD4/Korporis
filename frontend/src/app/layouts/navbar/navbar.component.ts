afcdb6d3314ec64efe81d74674cc4b5bf45f01bc
RenatoXD4 <re.chicahual@duocuc.cl>
2023-12-09 20:20:38 -0300

    Revert "Intentando que funcione la base de datos xd"
    This reverts commit 56b50768f04d51b9562aa0fab8acfd87cb49ed3d.

--- frontend/src/app/layouts/navbar/navbar.component.ts
+++ frontend/src/app/layouts/navbar/navbar.component.ts
@@ -1,8 +1,6 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Subscription } from 'rxjs';
 import { ApibackendService } from 'src/app/services/apibackend.service';
-import { SharedService } from 'src/app/services/shared.service';
 
 
 @Component({
@@ -10,81 +8,39 @@ import { SharedService } from 'src/app/services/shared.service';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit, OnDestroy {
+export class NavbarComponent {
 
   especialidades: any[] = [];
   subespecialidades: any[] = [];
-  subespecialidadesPorEspecialidad: { [key: string]: any[] } = {};
-
-  private especialidadesSubscription: Subscription | undefined;
 
 
-  constructor(private api: ApibackendService, private sharedService: SharedService){
+  constructor(private api: ApibackendService, private router: Router){
 
   }
 
   ngOnInit(): void {
-    // Obtener las especialidades al inicializar el componente
+    // Obtener todas las especialidades
     this.api.obtenerEspecialidades().subscribe(
-      (especialidades) => {
-        // Actualizar las especialidades en el servicio compartido
+      (especialidades: any) => {
         this.especialidades = especialidades;
-        this.sharedService.updateEspecialidades(especialidades);
-        // Suscribirse a los cambios en las especialidades
-        this.especialidadesSubscription = this.sharedService.especialidades$.subscribe(
-          (especialidadesActualizadas) => {
-            this.especialidades = especialidadesActualizadas;
-          }
-        );
       },
-      (error) => {
-        console.error('Error al obtener especialidades:', error.message);
+      error => {
+        console.error('Error al obtener especialidades:', error);
       }
     );
   }
 
-  ngOnDestroy(): void {
-    // Desuscribirse para evitar posibles fugas de memoria
-    if (this.especialidadesSubscription) {
-      this.especialidadesSubscription.unsubscribe();
-    }
-  }
-
-
-  obtenerSubespecialidades(especialidadNombre: string): void {
-    console.log('Iniciando la obtención de subespecialidades...');
-    this.api.obtenerSubespecialidades(especialidadNombre).subscribe(
-      (response: any) => {
-        if (response && response.subespecialidades && Array.isArray(response.subespecialidades)) {
-          console.log('Subespecialidades:', response.subespecialidades);
-  
-          // Almacena las subespecialidades en el objeto usando el nombre de la especialidad como clave
-          this.subespecialidadesPorEspecialidad[especialidadNombre] = response.subespecialidades;
-        } else {
-          console.error('La respuesta del servidor no contiene subespecialidades válidas:', response);
-        }
+  // Método para obtener subespecialidades por especialidad
+  obtenerSubespecialidades(especialidadId: number): void {
+    this.api.obtenerSubespecialidadPorID(especialidadId).subscribe(
+      (subespecialidades: any) => {
+        this.subespecialidades = subespecialidades;
       },
-      (error) => {
+      error => {
         console.error('Error al obtener subespecialidades:', error);
       }
     );
   }
-
-
-  
-  toggleDropdown(especialidadId: number, especialidadNombre: string): void {
-    const dropdown = document.getElementById('doubleDropdown' + especialidadId);
-    if (dropdown) {
-      dropdown.classList.toggle('hidden');
-  
-      // Llama a la función para obtener las subespecialidades
-      if (!this.subespecialidadesPorEspecialidad[especialidadNombre]?.length) {
-        this.obtenerSubespecialidades(especialidadNombre);
-      }
-    }
-  }
-
-  // Método para obtener subespecialidades por especialidad
 
 
 }
--- frontend/src/app/services/apibackend.service.ts
+++ frontend/src/app/services/apibackend.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { SharedService } from './shared.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,38 +9,17 @@ export class ApibackendService {
 
   private apiUrl = 'http://localhost:8000/api/datos/';
 
-  constructor(private http: HttpClient, private sharedDatService: SharedService) {}
+  constructor(private http: HttpClient) {}
 
-  obtenerEspecialidades(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}especialidades`);
+  obtenerSubespecialidadPorNombre(nombre: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}subespecialidades/?nombre=${nombre}`);
   }
 
-  obtenerSubespecialidades(especialidadNombre: string): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}subespecialidades/${encodeURIComponent(especialidadNombre)}`);
+  obtenerSubespecialidadPorID(id: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}subespecialidades/?nombre=${id}`);
   }
 
-  actualizarEspecialidades(): void {
-    this.obtenerEspecialidades().subscribe(
-      (especialidades) => {
-        // Actualizar el servicio compartido con las nuevas especialidades
-        this.sharedDatService.updateEspecialidades(especialidades);
-      },
-      (error) => {
-        console.error('Error al obtener especialidades:', error.message);
-      }
-    );
+  obtenerEspecialidades(): Observable<any> {
+    return this.http.get(`${this.apiUrl}especialidades`);
   }
-
-  actualizarSubespecialidades(especialidadNombre: string): void {
-    this.obtenerSubespecialidades(especialidadNombre).subscribe(
-      (subespecialidades) => {
-        // Actualizar el servicio compartido con las nuevas subespecialidades
-        this.sharedDatService.updateSubespecialidades(subespecialidades);
-      },
-      (error) => {
-        console.error('Error al obtener subespecialidades:', error.message);
-      }
-    );
-  }
-
 }
