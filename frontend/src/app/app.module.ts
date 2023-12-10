e69c5d98a20ad6e4d2e59b3bf3b7da7ad14549b4
RenatoXD4 <re.chicahual@duocuc.cl>
2023-12-09 22:52:15 -0300

    Cambios

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,11 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ServiciosModule } from './views/servicios/servicios.module';
 import { SharedModule } from './module/common-module/common-module.module';
-import { ServiciosMusculoModule } from './views/serviciomusculo/servicios-musculo.module';
 
 
 
