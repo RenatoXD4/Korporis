3f89e9166731fd0b852b9fdb3f711227ab5d2e6e
RenatoXD4 <re.chicahual@duocuc.cl>
2023-12-19 18:13:43 -0300

    Borrando el backend

--- backend/servicios/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Especialidad, Subespecialidad
-
-# Register your models here.
-admin.site.register(Especialidad)
-admin.site.register(Subespecialidad)
