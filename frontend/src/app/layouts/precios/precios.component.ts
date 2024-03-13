cb1f30ffb0d3f1b4e130806b8cdcb62b6a15ab7e
RenatoXD4 <re.chicahual@duocuc.cl>
2024-03-13 00:12:29 -0300

    Cuadros de los precios, que difícil pa quiero morir unos 10 mins y revivir

--- frontend/src/app/layouts/precios/precios.component.ts
+++ frontend/src/app/layouts/precios/precios.component.ts
@@ -7,6 +7,7 @@ interface Service {
   clockIcon: string;
   moneyIcon: string;
   categoria: string;
+  image: string;
 }
 
 
@@ -20,11 +21,11 @@ export class PreciosComponent {
   categoriaSeleccionada = 'Kine'
 
   services: Service[] = [
-    { name: "Masaje descontracturante", time: "40 minutos", price: "25.000", clockIcon: "clock2.svg", moneyIcon: "dinero.svg", categoria: 'Kine' },
-    { name: "Drenaje linfático", time: "40 minutos", price: "22.000", clockIcon: "clock2.svg", moneyIcon: "dinero.svg", categoria: 'Kine' },
-    { name: "Masaje deportivo", time: "30 minutos", price: "23.000", clockIcon: "clock2.svg", moneyIcon: "dinero.svg", categoria: 'Kine' },
-    { name: "Masaje relajante", time: "30 minutos", price: "20.000", clockIcon: "clock2.svg", moneyIcon: "dinero.svg", categoria: 'Maso' },
-    { name: "Masaje post operatorio", time: "30 minutos", price: "20.000", clockIcon: "clock2.svg", moneyIcon: "dinero.svg", categoria: 'Fono' },
+    { name: "Masaje descontracturante", time: "40 minutos", price: "25.000", clockIcon: "clock2.svg", moneyIcon: "dinero.svg", categoria: 'Kine', image: 'massage.svg' },
+    { name: "Drenaje linfático", time: "40 minutos", price: "22.000", clockIcon: "clock2.svg", moneyIcon: "dinero.svg", categoria: 'Kine', image: 'massage.svg' },
+    { name: "Masaje deportivo", time: "30 minutos", price: "23.000", clockIcon: "clock2.svg", moneyIcon: "dinero.svg", categoria: 'Kine', image: 'massage.svg' },
+    { name: "Masaje relajante", time: "30 minutos", price: "20.000", clockIcon: "clock2.svg", moneyIcon: "dinero.svg", categoria: 'Maso', image: 'massage.svg' },
+    { name: "Masaje post operatorio", time: "30 minutos", price: "20.000", clockIcon: "clock2.svg", moneyIcon: "dinero.svg", categoria: 'Fono', image: 'massage.svg' },
     // Agrega más servicios si es necesario
   ];
 
