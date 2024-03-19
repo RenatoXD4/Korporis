37718cb8a11ccb8a018685cd1285ea3e1b232aaf
RenatoXD4 <re.chicahual@duocuc.cl>
2024-03-18 21:53:15 -0300

    Añadido el link para agendar hora y eliminar algunos svg

--- frontend/src/app/layouts/precios/precios.component.ts
+++ frontend/src/app/layouts/precios/precios.component.ts
@@ -1,14 +1,5 @@
 import { Component } from '@angular/core';
 
-interface Service {
-  name: string;
-  time: string;
-  price: string;
-  clockIcon: string;
-  moneyIcon: string;
-  categoria: string;
-  image: string;
-}
 
 
 @Component({
@@ -18,15 +9,5 @@ interface Service {
 })
 export class PreciosComponent {
 
-  categoriaSeleccionada = 'Kine'
-
-  services: Service[] = [
-    { name: "Masaje descontracturante", time: "40 minutos", price: "25.000", clockIcon: "clock2.svg", moneyIcon: "dinero.svg", categoria: 'Kine', image: 'massage.svg' },
-    { name: "Drenaje linfático", time: "40 minutos", price: "22.000", clockIcon: "clock2.svg", moneyIcon: "dinero.svg", categoria: 'Kine', image: 'massage.svg' },
-    { name: "Masaje deportivo", time: "30 minutos", price: "23.000", clockIcon: "clock2.svg", moneyIcon: "dinero.svg", categoria: 'Kine', image: 'massage.svg' },
-    { name: "Masaje relajante", time: "30 minutos", price: "20.000", clockIcon: "clock2.svg", moneyIcon: "dinero.svg", categoria: 'Maso', image: 'massage.svg' },
-    { name: "Masaje post operatorio", time: "30 minutos", price: "20.000", clockIcon: "clock2.svg", moneyIcon: "dinero.svg", categoria: 'Fono', image: 'massage.svg' },
-    // Agrega más servicios si es necesario
-  ];
 
 }
