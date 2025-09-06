49cf72e63cbec994b7e49555d7646f4ee630f31c
RenatoXD4 <renatochicahual@gmail.com>
2025-09-06 19:50:36 -0400

    Modificaciones a botones y mejor responsividad. Cambios de precios y eliminación de precios.

--- frontend/src/app/layouts/navbar/navbar.component.ts
+++ frontend/src/app/layouts/navbar/navbar.component.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
-
+import {
+  ChangeDetectorRef,
+  Component,
+  HostListener,
+  OnInit,
+} from '@angular/core';
 
 @Component({
-    selector: 'app-navbar',
-    templateUrl: './navbar.component.html',
-    styleUrls: ['./navbar.component.scss'],
-    standalone: true
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+  standalone: true,
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  dropdownPlacement: string = 'bottom';
 
+  constructor(private cdr: ChangeDetectorRef) {}
+  ngOnInit(): void {
+    this.updateDropdownPlacement();
+  }
 
+  // Actualizar la colocación basado en el tamaño de pantalla
+  updateDropdownPlacement(): void {
+    if (window.innerWidth < 768) {
+      this.dropdownPlacement = 'bottom'; // Para móviles
+    } else {
+      this.dropdownPlacement = 'right-start'; // Para desktop
+    }
+    this.cdr.detectChanges();
+  }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    this.updateDropdownPlacement();
+  }
 }
